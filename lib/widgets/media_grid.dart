ce504fe23a4a74d6807fc68e4d24955cf8796968
CHITESHVARUN10 <109293694+CHITESHVARUN10@users.noreply.github.com>
2026-08-15 16:09:46 +0530

    selection logic

--- lib/widgets/media_grid.dart
+++ lib/widgets/media_grid.dart
@@ -4,9 +4,42 @@ import 'package:photo_manager/photo_manager.dart';
 
 import '../models/media_item.dart';
 
-class MediaGrid extends StatelessWidget {
-   const MediaGrid({super.key,required this.assets});
-final List<AssetEntity> assets;
+class MediaGrid extends StatefulWidget {
+  const MediaGrid({super.key, required this.assets});
+  final List<AssetEntity> assets;
+
+  @override
+  State<MediaGrid> createState() => _MediaGridState();
+}
+
+class _MediaGridState extends State<MediaGrid> {
+  final Set<int> _selectedIndices = {};
+
+  bool get isSelectionMode => _selectedIndices.isNotEmpty;
+  bool _isSelected(int index) => _selectedIndices.contains(index);
+
+  void _toggleSelection(int index) {
+    setState(() {
+      if (_selectedIndices.contains(index)) {
+        _selectedIndices.remove(index);
+      } else {
+        _selectedIndices.add(index);
+      }
+    });
+  }
+
+  void _onItemTap(int index) {
+    if (isSelectionMode) {
+      _toggleSelection(index);
+    } else {
+      // Normal mode: Preview dialog will be handled here
+    }
+  }
+
+  void _onItemLongPress(int index) {
+    _toggleSelection(index);
+  }
+
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
@@ -16,19 +49,13 @@ final List<AssetEntity> assets;
         crossAxisSpacing: 10.0, // Space between columns
         childAspectRatio: 1.0, // Width-to-height ratio of each tile
       ),
-      itemCount: assets.length,
+      itemCount: widget.assets.length,
       itemBuilder: (context, index) {
-        return  MediaItem(
-          asset: assets[index],
-          isSelected: false,
-          onTap: () {
-            print("tapped $index");
-            
-          },
-          onLongPress: () {
-            print("long pressed $index");
-            
-          },
+        return MediaItem(
+          asset: widget.assets[index],
+          isSelected: _isSelected(index),
+          onTap: () => _onItemTap(index),
+          onLongPress: () => _onItemLongPress(index),
         );
       },
     );
